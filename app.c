bbcc1fdc939176d5818151f7d5ae2d5b1d4e1f7a
Sharnabho <sharnabho2018@gmail.com>
2023-09-17 21:28:37 +0530

    2nd Commit

--- app.c
+++ app.c
@@ -2,5 +2,6 @@
 int main()
 {
     printf("hello world");
+    printf("Namaste India!!!");
     return 1001;
 }
---
+++ readme.md
@@ -0,0 +1,3 @@
+# This is our Smart India Hackathon project
+
+thank you for visiting us
